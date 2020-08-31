2f0c4e2e5b6848903b0e9dd78a316fa63fc4ed44
smadduri5 <smadduri5@IN-5CG0253HJ8>
2020-08-31 14:33:02 +0530

    one loine of codesample project with

---
+++ SampleExercise/src/com/dxc/practice/Demo_01.java
@@ -0,0 +1,10 @@
+package com.dxc.practice;
+
+public class Demo_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("sample exercise by msv");
+	}
+
+}
